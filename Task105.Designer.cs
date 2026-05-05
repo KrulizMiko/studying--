ce0241c7a0668c7f4713625b3abc1decf160b50a
Артем <168135876+KrulizMiko@users.noreply.github.com>
2026-05-05 22:27:35 +0300

    105 page is done

--- Task105.Designer.cs
+++ Task105.Designer.cs
@@ -29,11 +29,15 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(128, 170);
+            this.button1.Location = new System.Drawing.Point(130, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(66, 36);
             this.button1.TabIndex = 0;
@@ -41,20 +45,74 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(221, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "0 speed";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(221, 127);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "3th speed";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1st speed",
+            "2nd speed"});
+            this.comboBox1.Location = new System.Drawing.Point(197, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Task105
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(330, 348);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Task105";
             this.Text = "Task105";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Task105.cs
+++ Task105.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,24 +18,99 @@ namespace MDK06._03
             InitializeComponent();
         }
 
+        bool isRunning = false;
+
+
+        private int GetDelay()
+        {
+            // 0 speed 
+            if (checkBox1.Checked)
+                return 100; //100ms = 10 милисекунд = 0.1 секунда
+
+            // 1 / 2 speed
+            if (comboBox1.SelectedItem != null)
+            {
+                if (comboBox1.SelectedItem.ToString() == "1st speed")
+                    return 50;//50ms =  50 милисекунд = 0.05
+
+                if (comboBox1.SelectedItem.ToString() == "2nd speed")
+                    return 10;//10ms =  10 милисекунд = 0.01
+            }
+
+            // 3 speed 
+            if (radioButton1.Checked)
+                return 0; //0ms - без задержкит = 0 секунд
+
+            return 10;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-          
-            while (Visible)
-            {
-                for (int c = 0; c < 254 && Visible; c++)
+
+            isRunning = true;
+
+            while (isRunning)
+            {     
+                for (int c = 0; c < 254 && isRunning; c++)
                 {
                     this.BackColor = Color.FromArgb(c, 255 - c, c);
                     Application.DoEvents();
-                    System.Threading.Thread.Sleep(3);
+                    System.Threading.Thread.Sleep(GetDelay());
                 }
 
-                for (int c = 254; c >= 0 && Visible; c--)
+                for (int c = 254; c >= 0 && isRunning; c--)
                 {
                     this.BackColor = Color.FromArgb(c, 255 - c, c);
                     Application.DoEvents();
-                    System.Threading.Thread.Sleep(3);
+                    System.Threading.Thread.Sleep(GetDelay());
                 }
+
+                Application.DoEvents(); 
+            }
+        }
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            isRunning = false;
+
+            // сброс элементов
+            checkBox1.Checked = false;
+            radioButton1.Checked = false;
+            comboBox1.SelectedIndex = -1;
+
+            // вернуть стандартный цвет
+            this.BackColor = SystemColors.Control;
+        }
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex != -1)
+            {
+                checkBox1.Checked = false;
+                radioButton1.Checked = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+              if (checkBox1.Checked)
+              {
+                  comboBox1.SelectedIndex = -1;
+                  radioButton1.Checked = false;
+              }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                checkBox1.Checked = false;
+                comboBox1.SelectedIndex = -1;
             }
         }
     }
